34c888bbf1eb184a0f8ce61b1f91555cf78603de
nahuelmol <molinahuel44@gmail.com>
2023-12-22 01:36:09 -0300

    fixing the persistent issue

--- db/conn.js
+++ db/conn.js
@@ -2,7 +2,7 @@ const { Client } = require('pg');
 
 const client = new Client({
   user: process.env.PG_USER,
-  host: process.env.PG_HOST,
+  host: '127.0.0.1',
   database: process.env.PG_DATABASE,
   password: process.env.PG_PASSWORD,
   port: process.env.PG_PORT,
