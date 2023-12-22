965c7b39aefbedab06e82a685db46622a745e415
nahuelmol <molinahuel44@gmail.com>
2023-12-22 01:47:11 -0300

    postgresql trying to reinstall environment variables

--- db/conn.js
+++ db/conn.js
@@ -2,7 +2,7 @@ const { Client } = require('pg');
 
 const client = new Client({
   user: process.env.PG_USER,
-  host: '127.0.0.1',
+  host: process.env.PG_HOST,
   database: process.env.PG_DATABASE,
   password: process.env.PG_PASSWORD,
   port: process.env.PG_PORT,
