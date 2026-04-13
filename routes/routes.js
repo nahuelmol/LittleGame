325a6e2e4c86bfd2ed833d9644cf257a87acb011
unknown <kindermolina98@gmail.com>
2026-04-12 22:03:34 -0300

    adding personal projects;

--- routes/routes.js
+++ routes/routes.js
@@ -9,6 +9,10 @@ router.get('/register', async (req, res) => {
     res.render('register')
 })
 
+router.get('/projects', async (req, res) => {
+    res.render('projects')
+})
+
 router.get('/contact', async (req, res) => {
     res.render('contacts')
 })
